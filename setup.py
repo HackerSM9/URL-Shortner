20989c58b581866c1dffe061e28f9e399141b6fa
Samarth Magdum (SM9) <101000624+HackerSM9@users.noreply.github.com>
2022-07-28 13:23:55 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,9 @@
+import os,sys
 
+os.system("clear")
+os.system("pkg install python python2 python3 curl -y")
+os.system("pip3 install lolcat || pip2 install lolcat")
+os.system("clear")
+os.system("")
+print("")
+print("")
