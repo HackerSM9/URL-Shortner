d7853f8c1406612f585827347f18b12dc3bb7d9f
Samarth Magdum (SM9) <101000624+HackerSM9@users.noreply.github.com>
2022-07-29 20:55:39 +0530

    Update .magic.py

--- .src/.magic.py
+++ .src/.magic.py
@@ -1,6 +1,6 @@
 import os,sys
 
 print("")
-url = input(" [?] Enter Full URL Address: ")
-print(" [!] Your Link is Ready: ")
+url = input("[?] Enter Full URL Address: ")
+print("[!] Your Link is Ready: ")
 os.system("curl https://tinyurl.com/api-create.php?url={0}\n\n".format(url))
