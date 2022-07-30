446bb49030a0af7011a2e6588b8425f938893174
Samarth Magdum (SM9) <101000624+HackerSM9@users.noreply.github.com>
2022-07-30 13:58:46 +0530

    Update .magic.py

--- .src/.magic.py
+++ .src/.magic.py
@@ -1,17 +1,11 @@
 import os,sys
 
-r = '\033[1;31m'
-g = '\033[0;32m'
-y = '\033[1;33m'
-b = '\033[0;34m'
-v = '\033[0;35m'
-e = '\033[0m'
-
-q = '\033[0;33m[\033[0;31m?\033[0;33m]\033[0m'
+q = '\033[0;33m[\033[1;31m?\033[0;33m]\033[0m'
 i = '\033[0;33m[\033[0;32m!\033[0;33m]\033[0m'
 
 print("")
 url = input(q+" Enter Full URL Address: ")
+print("")
 print(i+" Your Link is Ready: \033[1;34m ", end = "", flush = True)
 os.system("curl https://tinyurl.com/api-create.php?url={0}".format(url))
 print("\n")
