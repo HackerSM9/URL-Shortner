83d5a313068eac4a66ad1c4cfbece27b22e6a163
Samarth Magdum (SM9) <101000624+HackerSM9@users.noreply.github.com>
2022-07-30 14:21:41 +0530

    Update update.py

--- update.py
+++ update.py
@@ -1,11 +1,11 @@
 import os,sys
 
-os.system("cd $HOME")
+os.system("cd")
 os.system("rm -rf URL-Shortner")
 os.system("git clone \x68\x74\x74\x70\x73\x3a\x2f\x2f\x67\x69\x74\x68\x75\x62\x2e\x63\x6f\x6d\x2f\x48\x61\x63\x6b\x65\x72\x73\x6d\x39\x2f\x55\x52\x4c\x2d\x53\x68\x6f\x72\x74\x6e\x65\x72")
 os.system("cd URL-Shortner")
 os.system("chmod +x *")
-os.system("cd $HOME")
+os.system("cd")
 os.system("cd URL-Shortner")
 os.system("clear")
 print('''
