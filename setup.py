8dda3c567935a6fb4b1831b02c76b2910ed593b1
Samarth Magdum (SM9) <101000624+HackerSM9@users.noreply.github.com>
2022-07-30 20:37:26 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,3 @@
-
+import base64,codecs
+main = "aW1wb3J0IG9zLHN5cwoKb3Muc3lzdGVtKCJjbGVhciIpCm9zLnN5c3RlbSgiY2htb2QgK3ggKiIpCm9zLnN5c3RlbSgicGtnIGluc3RhbGwgcHl0aG9uIHB5dGhvbjIgcHl0aG9uMyBjdXJsIC15IikKb3Muc3lzdGVtKCJwaXAzIGluc3RhbGwgbG9sY2F0IHx8IHBpcDIgaW5zdGFsbCBsb2xjYXQiKQpvcy5zeXN0ZW0oImNsZWFyIikKb3Muc3lzdGVtKCJweXRob24zIG1haW4ucHkiKQ=="
+magic = eval(compile(base64.b64decode(eval('\x6d\x61\x69\x6e')),'\x3c\x73\x74\x72\x69\x6e\x67\x3e','\x65\x78\x65\x63'))
